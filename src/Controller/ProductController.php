88f74c644483007ffb14671acaa12db2fe562d20
Paul Piotrowski <paul.piotro@gmail.com>
2021-03-05 16:31:15 +0100

    panier paiement

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
-//use App\Form\SearchForm;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use App\Repository\ProductRepository;
@@ -16,26 +17,17 @@ use App\Repository\ProductRepository;
 
 class ProductController extends AbstractController
 {
-   /* public function categorie(Environment $twig)
+
+
+
+    public function product(Environment $twig,ProductRepository $produitRepository, SessionInterface $session)
     {
 
-        $content = $twig->render('product/categorie.html.twig');
-        return new Response($content);
-    }*/
-
-
-    public function product(Environment $twig,ProductRepository $produitRepository, Request $request)
-    {
-        //$data = new SortData();
-        //$form = $this->createForm(SearchForm::class, $data);
-        //$form->handleRequest($request);
-
-        // $produits = $this->maCategorie($produitRepository, $id);
         $produits = $produitRepository->findAll();
 
         return $this->render('product.html.twig', [
             'products' => $produits
-           // 'form' => $form->createView()
+
         ]);
     }
 
